38e11e9144e996fd54dcbd8b8edb7cb96149fdeb
Daniela Eloisa <danielaeloisa1@gmail.com>
2024-05-08 15:38:04 -0300

    Ajustes no menu lateral da Pagina Inicial; ajustes no crud de Publicações

--- src/js/paginaInicial.js
+++
@@ -1,9 +0,0 @@
-
-function mudarVisibilidade(){
-    var menuLateral= document.getElementsByClassName('menu_superior');
-    console.log(menuLateral)
-    if(menuLateral.style.visibility)
-        menuLateral.style.visibility= visible;
-    else
-    menuLateral.style.visibility= hidden;
-}

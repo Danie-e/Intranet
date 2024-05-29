6033e4ddb17ee8a2772651d5e00155953c9722be
Daniela Eloisa <danielaeloisa1@gmail.com>
2024-05-29 11:18:43 -0300

    Ajuste na APi

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -1,10 +1,11 @@
 import express from "express";
 import usuarios from "./usuariosRoutes.js";
 import publicacao from "./publicacaoRoutes.js"
+import pdi from "./PDIRoutes.js"
 
 const routes = (app) => {
     app.route("/").get((req,res)=> res.status(200).send("curso de Node.js"));
-    app.use(express.json(),usuarios,publicacao);
+    app.use(express.json(),usuarios,publicacao,pdi);
 };
 
 export default routes;

cc76f87bb58566a5cbef69b283d1f6e9f180fae7
Daniela Eloisa <danielaeloisa1@gmail.com>
2024-05-29 11:36:55 -0300

    Ajuste na mensagem de resposta da API

--- src/js/criarpdi.js
+++ src/js/criarpdi.js
@@ -6,13 +6,13 @@ async function salvarPDI() {
     var subtitulo = document.getElementById('adcArea').value;
     var conteudo = document.getElementById('adcConteudo')
     var prazo = document.getElementById('adcPrazo').value;
-    var tituloTarefa1 = document.getElementsByid('cAdcTarefa1').value;
+    var tituloTarefa1 = document.getElementById('cAdcNomeTarefa1').value;
     var linkTarefa1 = document.getElementById('cAdcLink1').value;
 
 
     var publicacoes = {
         titulo: titulo,
-        subtitulo: subtitulo, 
+        subtitulo: subtitulo,
         conteudo: conteudo,
         prazo: prazo,
     }
@@ -22,14 +22,15 @@ async function salvarPDI() {
         headers: {
             "Content-type": "application/json"
         },
-        body: JSON.stringify(pdi)
-    })
-    alert(result.json());
-    if (result.status==200) {
+        body: JSON.stringify(publicacoes)
+    });
+    let resultado = await result.json()
+    alert();
+    if (resultado.status == 200) {
         alert("PDI criado com sucesso");
         window.open("./meusPdis.html");
     }
     else {
-        alert("Não foi possivel criar publicação!");
+        alert(`${resultado.message} Não foi possivel criar PDI!`);
     }
 }

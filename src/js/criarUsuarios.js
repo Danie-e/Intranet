1b2721e3085192745561e83761538953bad22534
Daniela Eloisa <danielaeloisa1@gmail.com>
2024-05-29 20:12:34 -0300

    Ajustar requisição  criar usuario

--- src/js/criarUsuarios.js
+++ src/js/criarUsuarios.js
@@ -1,27 +1,28 @@
 document.addEventListener("submit", async function () {
-    const nome = document.getElementById("nome");
-    const senha = document.getElementById("senha");
-    const equipe = document.getElementById("equipe");
-    const tipo = document.getElementById("tipo");
-    const imagem = document.getElementById("foto");
+    const nome = document.getElementById("nome").value;
+    const senha = document.getElementById("senha").value;
+    const equipe = document.getElementById("equipe").value;
+    const tipo = document.getElementById("tipo").value;
+    const imagem = document.getElementById("foto").value;
 
-    const novoUsuario = {
+    var novoUsuario = {
         nome: nome,
         senha: senha,
         equipe: equipe,
         tipo: tipo,
         imagem: imagem
-    }
+    };
 
-    alert("enviando");
     const resultado = await fetch("http://localhost:3000/usuarios", {
         method: "POST",
         headers: {
             "Content-type": "application/json"
         },
-        body: JSON.stringify(novoUsuario)
+        body: JSON.stringify(novoUsuario),
     });
-    if (resultado.ok) {
+    var result = resultado.json();
+    alert(result);
+    if (result.ok) {
         alert(resultado.message);
     }
     else

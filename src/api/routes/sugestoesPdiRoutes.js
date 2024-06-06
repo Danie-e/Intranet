84bd6ac91a81962b482f4f3171fc54a164635318
Letícia Gabriela Ramos Macedo <lettsramos@gmail.com>
2024-06-06 09:44:58 -0300

    Correcao

--- src/api/routes/sugestoesPdiRoutes.js
+++ src/api/routes/sugestoesPdiRoutes.js
@@ -6,7 +6,7 @@ const routes = express.Router();
 // Rotas para PDI
 routes.get("/sugestoespdi", sugestoesPDIController.listarsugestoesPDI);
 routes.get("/sugestoespdi/:id", sugestoesPDIController.procurarsugestoesPDI); // pelo id do usuário
-routes.post("/sugestoespdi", sugestoesPDIController.sugestoescadastrarPDI);
+routes.post("/sugestoespdi", sugestoesPDIController.cadastrarsugestoesPDI);
 //routes.put("/pdi/:id", PDIController.atualizarPDI);
 //routes.delete("/pdi/:id", PDIController.excluirPDI);
 

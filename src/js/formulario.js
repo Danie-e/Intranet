f83c4dd859fe802ae9ce230610db538b02612046
Daniela Eloisa <danielaeloisa1@gmail.com>
2024-06-07 21:58:53 -0300

    Ajustes nos menus

--- src/js/formulario.js
+++ src/js/formulario.js
@@ -3,42 +3,43 @@ import { funcoes } from "./funcoes.js";
 document.addEventListener("DOMContentLoaded", async function () {
     const idFormulario = funcoes.acharCookie("idFormulario=")
     const dadosFormulario = await fetch(`http://localhost:3000/formulario/${idFormulario}`);
-    console.log(dadosFormulario);
-    document.title.value = dadosFormulario.titulo;
-    const campos = dadosFormulario.formHtml.split(" ");
-    alert(campos);
+    const elemento = await dadosFormulario.json();
 
+    console.log(elemento);
 
-    buttonCriar.onclick = async function () {
-        const nomeFormulario = document.getElementById('nomeFormulario').value;
-        let formulario = document.getElementById('criarFormularios');
+    document.getElementById('nomePagina').innerHTML = elemento.titulo;
+    document.getElementById('nomeFormulario').innerHTML = elemento.titulo;
 
-        let valores = [].map.call(formulario, function (input) {
-            return ` ${input.name} ${input.value}`;
-        });
-        const autor = funcoes.acharCookie("id=");
-        const corpo = {
-            titulo: nomeFormulario,
-            autor: autor,
-            formHtml: conteudo,
-            conteudo: valores
-        }
-        console.log(await JSON.stringify(corpo));
+    // buttonCriar.onclick = async function () {
+    //     const nomeFormulario = document.getElementById('nomeFormulario').value;
+    //     let formulario = document.getElementById('criarFormularios');
 
-        const result = await fetch("http://localhost:3000/formulario", {
-            method: "POST",
-            headers: {
-                "Content-type": "application/json"
-            },
-            body: JSON.stringify(corpo),
-        })
-        const resultado = await result.json();
-        if (resultado.status == 201) {
-            alert(resultado.message);
-            window.location.href = './paginaInicial.html';
-        }
-        else {
-            alert(resultado.message);
-        }
-    }
+    //     let valores = [].map.call(formulario, function (input) {
+    //         return ` ${input.name} ${input.value}`;
+    //     });
+    //     const autor = funcoes.acharCookie("id=");
+    //     const corpo = {
+    //         titulo: nomeFormulario,
+    //         autor: autor,
+    //         formHtml: conteudo,
+    //         conteudo: valores
+    //     }
+    //     console.log(await JSON.stringify(corpo));
+
+    //     const result = await fetch("http://localhost:3000/formulario", {
+    //         method: "POST",
+    //         headers: {
+    //             "Content-type": "application/json"
+    //         },
+    //         body: JSON.stringify(corpo),
+    //     })
+    //     const resultado = await result.json();
+    //     if (resultado.status == 201) {
+    //         alert(resultado.message);
+    //         window.location.href = './paginaInicial.html';
+    //     }
+    //     else {
+    //         alert(resultado.message);
+    //     }
+    // }
 });

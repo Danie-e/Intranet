af4d27955631729c341a3f1792de672cedf28533
Daniela Eloisa <danielaeloisa1@gmail.com>
2024-06-11 20:26:40 -0300

    Ajuste menus

--- src/api/config/dbConnect.js
+++
@@ -1,9 +0,0 @@
-import mongoose, { mongo } from "mongoose";
-
-async function connect() {
-  mongoose.connect("mongodb+srv://Admin:Admin@intranet.dclizxs.mongodb.net/Intranet?retryWrites=true&w=majority&appName=Intranet");
- 
-  return mongoose.connection;
-};
-
-export default connect;
